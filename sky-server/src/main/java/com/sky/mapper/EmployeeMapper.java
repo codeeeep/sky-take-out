1af14c4dc6a19e9822aa1872eed705378038a7fb
程瑜 <2479676487@qq.com>
2023-11-20 23:52:54 +0800

    feat: add employee page query

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -17,10 +19,20 @@ public interface EmployeeMapper {
     Employee getByUsername(String username);
 
 
+    /**
+     * 新增员工信息
+     * @param employee
+     */
     @Insert("insert into employee (name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user) "
             + "values "
             + "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
     void insert(Employee employee);
 
 
+    /**
+     * 根据员工姓名查询
+     * @param employeePageQueryDTO
+     * @return
+     */
+    Page pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
