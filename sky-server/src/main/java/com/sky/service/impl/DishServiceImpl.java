05615cefed3b2b4bdf71c005a6b9c21cd415e32a
程瑜 <2479676487@qq.com>
2023-11-24 22:01:05 +0800

    feat: add update dish

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -84,7 +84,7 @@ public class DishServiceImpl implements DishService {
      * 菜品批量删除
      * @param ids
      */
-    @Transactional
+    @Transactional(rollbackFor = Exception.class)
     @Override
     public void deleteBatch(List<Long> ids) {
         // 判断当前菜品是否能够删除 —— 是否存在起售中的菜品 ？
@@ -108,5 +108,50 @@ public class DishServiceImpl implements DishService {
         // sql: delete from dish_flavor where dish_id in (?,?,?)
         dishFlavorMapper.deleteByDishIds(ids);
 
+    }
+
+    /**
+     * 根据 id 查询菜品和对应的口味数据
+     * @param id
+     * @return
+     */
+    @Override
+    public DishVO getByIdWithFlavor(Long id) {
+        // 先获取到菜品数据
+        Dish dish = dishMapper.getById(id);
+
+        // 再获取到对应的口味数据
+        List<DishFlavor> dishFlavors = dishFlavorMapper.getByDishId(id);
+
+        // 将查询到的数据封装到 VO 对象中
+        DishVO dishVO = new DishVO();
+        BeanUtils.copyProperties(dish, dishVO);
+        dishVO.setFlavors(dishFlavors);
+
+        return dishVO;
+    }
+
+    /**
+     * 根据 id 修改对应的菜品信息和相应的口味信息
+     * @param dishDTO
+     */
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public void updateWithFlavor(DishDTO dishDTO) {
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO, dish);
+        // 修改菜品的基础信息
+        dishMapper.update(dish);
+
+        // 删除原有的口味信息
+        dishFlavorMapper.deleteByDishId(dishDTO.getId());
+
+        // 重新批量插入的口味的信息
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        if (flavors != null && !flavors.isEmpty()) {
+            flavors.forEach(dishFlavor -> dishFlavor.setDishId(dishDTO.getId()));
+            // 向口味表插入 n 条数据
+            dishFlavorMapper.insertBatch(flavors);
+        }
     }
 }
